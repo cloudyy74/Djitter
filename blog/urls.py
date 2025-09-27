b36609beaa3e79fe0634253ff274f0d844f9d13c
Vitas Remeykis <ccloudyy74@gmail.com>
2025-09-27 23:15:57 +0300

    feat: make blog app main and added about page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,16 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 from django.urls import path
+
 from . import views
 
+
+def root_redirect(request: HttpRequest) -> HttpResponse:
+    return redirect('/home/')
+
+
 urlpatterns = [
-   path('', views.home, name='blog-home') 
+    path('home/', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('', root_redirect, name='blog-root'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<h1>Blog Home<h1>')
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>About Djitter<h1>')
