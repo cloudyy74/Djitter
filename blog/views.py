6023719d3e90f6c493a368abdec6a98027630906
Vitas Remeykis <ccloudyy74@gmail.com>
2025-09-28 13:35:51 +0300

    feat: added some dummy posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'cat_cucumber',
+        'title': 'post 1',
+        'content': 'first post',
+        'date_posted': 'September 28, 2025',
+    },
+    {
+        'author': 'cat_tomato',
+        'title': 'post 2',
+        'content': 'second post',
+        'date_posted': 'September 29, 2025',
+    },
+]
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>About Djitter<h1>')
+    return render(request, 'blog/about.html')
