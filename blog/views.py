567b0f7059d2ebc84b5c35c59a8f2059a92b0f82
Vitas Remeykis <ccloudyy74@gmail.com>
2025-09-28 13:26:35 +0300

    feat: added templates for home page

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Blog Home<h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request: HttpRequest) -> HttpResponse:
