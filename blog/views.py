3aec7379e9064147a5b8763237e19475f858d711
Vitas Remeykis <ccloudyy74@gmail.com>
2025-09-28 17:37:33 +0300

    feat: replaced dummy posts with database+ORM

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'cat_cucumber',
-        'title': 'post 1',
-        'content': 'first post',
-        'date_posted': 'September 28, 2025',
-    },
-    {
-        'author': 'cat_tomato',
-        'title': 'post 2',
-        'content': 'second post',
-        'date_posted': 'September 29, 2025',
-    },
-]
+from .models import Post
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'blog/home.html', context)
+    return render(
+        request, 'blog/home.html', {'posts': Post.objects.order_by('-date_posted')}
+    )
 
 
 def about(request: HttpRequest) -> HttpResponse:
