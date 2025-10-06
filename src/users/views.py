ec6b018c8a29dc999226bb2c3b8e997e03b71da1
Vitas Remeykis <ccloudyy74@gmail.com>
2025-10-06 09:47:43 +0300

    feat: user update form in profile, href from article to author

--- src/users/views.py
+++ src/users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import UserRegisterForm
+from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
 
 
 def register(request: HttpRequest) -> HttpResponse:
@@ -23,4 +23,27 @@ def register(request: HttpRequest) -> HttpResponse:
 
 def profile(request: HttpRequest, username=str) -> HttpResponse:
     profile_user = get_object_or_404(User, username=username)
-    return render(request, 'users/profile.html', {'profile_user': profile_user})
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=profile_user)
+        profile_update_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=profile_user.profile
+        )
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(
+                request, 'Your account has been updated!'
+            )
+            return redirect('users-profile', username=profile_user.username)
+
+    else:
+        user_update_form = UserUpdateForm(instance=profile_user)
+        profile_update_form = ProfileUpdateForm(instance=profile_user.profile)
+
+    context = {
+        'profile_user': profile_user,
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form,
+    }
+
+    return render(request, 'users/profile.html', context)
